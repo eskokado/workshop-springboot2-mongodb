a8f1d80d3937c15ceaf3b21a92068a80dcb573c6
Edson Shideki Kokado <eskokado@gmail.com>
2019-06-23 07:20:50 -0300

    Obtendo um usuário por id

--- src/main/java/br/com/eskinfotechweb/workshopmongodb/services/UserService.java
+++ src/main/java/br/com/eskinfotechweb/workshopmongodb/services/UserService.java
@@ -1,20 +1,27 @@
 package br.com.eskinfotechweb.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.eskinfotechweb.workshopmongodb.domain.User;
 import br.com.eskinfotechweb.workshopmongodb.repositories.UserRepository;
+import br.com.eskinfotechweb.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
