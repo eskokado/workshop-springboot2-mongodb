52318683e4ab55686a4e10e2570b18a19a3ce66a
Edson Shideki Kokado <eskokado@gmail.com>
2019-06-23 09:03:38 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/br/com/eskinfotechweb/workshopmongodb/repositories/PostRepository.java
@@ -0,0 +1,11 @@
+package br.com.eskinfotechweb.workshopmongodb.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.eskinfotechweb.workshopmongodb.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
